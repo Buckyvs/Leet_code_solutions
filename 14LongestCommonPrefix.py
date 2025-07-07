610a5d4cef0c24713bc452f82269bde19f65a311
Buckyvs <shuklavishakha02@gmail.com>
2025-07-07 11:22:12 +0530

    renamed and added comments

--- 14LongestCommonPrefix.py
+++ 14LongestCommonPrefix.py
@@ -7,8 +7,12 @@ class Solution(object):
         if not strs:
             return ""
 
+        #loop through each character index of the first string
         for i in range(len(strs[0])):
+            # check if this character is the same in all strings
             for s in strs[1:]:
+                # if we reach the end of any string or the character doesn't match
+                # we return the common prefix found so far
                 if i >= len(s) or s[i] != strs[0][i]:
                     return strs[0][:i]
 
